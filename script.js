007be21136474e8740c2a31e959939c6d630617a
dokikoteswar <139772409+dokikoteswar@users.noreply.github.com>
2023-09-26 15:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let parent = document.getElementTagName("body");
+
+let p1 = document.getElementById("status");
+function btn() {
+	let n =document.createElement("h1");
+	// n.innerHTML =""
+	// p1.remove();
+	parent.replaceChild(n, p1);
+}
